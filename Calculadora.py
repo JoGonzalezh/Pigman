29715428dbd3b31af32402e40238f0ae1b5b3f6e
JoGonzalezh <changeme@example.com>
2025-03-28 16:38:03 +0000

    esta todo

--- Calculadora.py
+++ Calculadora.py
@@ -1,9 +1,14 @@
+from funciones.funciones import suma,resta,multi.dive
+
 print("Bienvenido")
 
 num1 = int(input("Ingrese el primer numero"))
 num2 = int(input("Ingrese el Segundo numero"))
 
-print(f"la suma de los 2 valores es {num1 + num2}")
-print(f"la resta de los 2 valores es {num1 - num2}")
+suma(num1, num2)
+resta(num1, num2)
+multi(num1, num2)
+dive(num1, num2)
+
 
 
